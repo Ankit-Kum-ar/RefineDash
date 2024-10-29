696bacfd6d734307aec1003a1203323fb285fd6c
Ankit-0009 <ankitcode2511@gmail.com>
2024-10-29 23:13:37 +0530

    Use Color-Mode for dark mode of application.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+import { ColorModeContextProvider } from "./contexts/color-mode";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorModeContextProvider>
+      <App />
+    </ColorModeContextProvider>
   </React.StrictMode>
 );
