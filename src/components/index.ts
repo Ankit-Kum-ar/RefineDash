298324043051871c9dd24d1bb2e66756687a18e4
Ankit-0009 <ankitcode2511@gmail.com>
2024-10-31 18:01:15 +0530

    Export all skeletons.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,14 @@
 export { Header } from "./header";
+
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+export { 
+    UpcomingEventsSkeleton, 
+    AccordionHeaderSkeleton, 
+    KanbanColumnSkeleton, 
+    LatestActivitiesSkeleton,
+    ProjectCardSkeleton 
+};
